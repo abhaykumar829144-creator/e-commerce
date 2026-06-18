17b009dc09f9d6d9dd335b303e4c4014f89e45ea
abhay318 <abhaykumar829144@gmail.com>
2026-06-18 18:00:29 +0530

    header updated

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,10 +1,27 @@
 "use client"
 import React from 'react'
 import "./Header.css"
+import { GoHomeFill } from 'react-icons/go'
+import { FaUser } from 'react-icons/fa'
+import { BsCart4 } from 'react-icons/bs'
+
+import "./Header.css";
 
 const Header : React.FC = () => {
   return (
-    <div>Header</div>
+    <>
+    <div className="main_header">
+      <div className="header_left">
+        <div className='header_left_icon'><GoHomeFill /></div>
+        <div className='header_title'>shop Ease</div>
+      </div>
+      
+        <div className="header_right">
+          <div className='header_right_icon'><FaUser /></div>
+          <div className='header_right_icon'><BsCart4 /></div>
+        </div>
+      </div>
+    </>
   )
 }
 
