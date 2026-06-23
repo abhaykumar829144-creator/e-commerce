f06a7608389cc35e18ded558d41a9f7da8c314c6
abhay318 <abhaykumar829144@gmail.com>
2026-06-23 18:35:39 +0530

    banner created

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import HomeBanner from "@/components/homeComponents/HomeBanner";
 import Header from "@/components/layout/Header";
 
 export default function Home() {
   return (
     <>
     <Header/>
+    <HomeBanner/>
     </>)
 }
